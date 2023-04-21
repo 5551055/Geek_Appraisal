284366e7c659c30498decfb5d1746240cbfe8d20
5551055 <2358643911@qq.com>
2023-04-21 17:26:30 +0800

    MOdify task1

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a,i,y;
+	double x;
+	//a=M_E;
+	//a=M_PI;
+    x=0;//设置x的初始值为0
+	float h=0.001;//定义步长
+	printf("请输入a的值");
+	scanf("%f",&a);
+	printf("x         y\n");
+	y=a;//定义y在x为0的值为a
+	printf("%lf  ",x);
+	printf("%f\n",y);
+	do
+	{
+		i=y;
+		y=y+h*(-y);
+		x+=h;
+		printf("%lf  ",x);
+		printf("%f\t",y);
+		printf("\n");
+	} while(fabs(y-i)>0.001*h);
+	printf("定时值为%f",y);
 	return 0;
 }
-
